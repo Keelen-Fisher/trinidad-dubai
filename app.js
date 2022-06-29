310a0c5512e1c3c807dd63c6b069d619fb08afc9
Keelen Fisher <keelencarrera@gmail.com>
2022-06-29 18:28:04 -0500

    Adding In JavaScritpt

---
+++ app.js
@@ -0,0 +1,68 @@
+// let bestcolor = "Green";
+// let otherbestcolor = "green";
+
+// let favcolor = bestcolor; 
+
+// console.log(favcolor);
+// console.log(otherbestcolor);
+
+// console.log(bestcolor == otherbestcolor);
+
+// let usersColor = prompt("What is your favorite color?");
+
+// console.log(usersColor);
+
+// let x = 5;
+// let y = "5";
+
+// // console.log(x, y);
+// // console.log(x == y);
+// // console.log(x === y);
+
+
+// if (usersColor.toLowerCase() == "green")
+// {
+//     console.log("That's my color too!");
+//     document.write("That's my color too!")
+// }
+
+// else if (usersColor.toLowerCase() == "yellow"){
+//     document.write("Mine too!")
+// }
+
+// else
+// {
+// console.log("Well that's still a nice color ")
+// document.write("Well that's still a nice color!")
+// }
+
+
+
+
+// let trini = "island";
+// let country = trini;
+
+// console.log(trini == country);
+// console.log("island");
+let usersinput = prompt("Do you prefer the island or the city?");
+
+console.log(usersinput);
+
+if (usersinput.toLowerCase() == "island")
+{
+console.log("Well then welcome to Trinidad!!");
+document.write("Well then welcome to Trinidad!!");
+}
+
+else if (usersinput.toLowerCase() == "iceland")
+{
+    console.log("Go over to Xavier's page fro more info!");
+    document.write("Go over to Xavier's page for more info!" ); 
+}
+
+
+else 
+{
+    console.log("Well then welcome to Dubai!");
+    document.write("Well then welcome to Dubai!!");
+}
